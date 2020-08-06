4547e15028a5678311b985e8b128285aeabc74c9
dave <lklone2005@gmail.com>
2020-08-06 19:24:34 +0900

    change BgPhoto codes

--- client/src/components/BgPhoto.js
+++ client/src/components/BgPhoto.js
@@ -1,38 +1,75 @@
-import React, { Component } from 'react';
+import React from "react";
+import styled from "styled-components";
 
+const BgphotoBlock = styled.div`
+  background-image: url(https://source.unsplash.com/category/nature/1600x900);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: -1;
+`;
+
+const BgphotoBlockCover = styled.div`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 1;
+`;
+
+const BgPhoto = () => {
+  return (
+    <BgphotoBlock>
+      <BgphotoBlockCover>
+        <div id="bgPhoto">
+          <div id="container">
+            <div className="bgPhoto"></div>
+          </div>
+        </div>
+      </BgphotoBlockCover>
+    </BgphotoBlock>
+  );
+};
+/*
 function BgPhotoInit() {
-    const backGroundImg = document.querySelector("#root .App .bgPhoto");
-    const unsplashUrl = "https://source.unsplash.com/category/nature/1600x900";
-  
-    function getImage() {
-      let bgImg = new Image();
-        bgImg.alt = "배경화면";
-        bgImg.src = unsplashUrl;
-        backGroundImg.appendChild(bgImg);
-    }
+  const backGroundImg = document.querySelector("#root .App .bgPhoto");
+  const unsplashUrl = "https://source.unsplash.com/category/nature/1600x900";
+  console.log(unsplashUrl);
 
-    function init(){
-      getImage();
-  
-    }
-  
-    init();
+  function getImage() {
+    let bgImg = new Image();
+    bgImg.alt = "배경화면";
+    bgImg.src = unsplashUrl;
+    backGroundImg.appendChild(bgImg);
+  }
+
+  function init() {
+    getImage();
+  }
+
+  init();
 }
 
 class BgPhoto extends Component {
-    componentDidMount() {
-        BgPhotoInit();
-    }
-    
-    render() {
-        return (
-            <div id="bgPhoto">
-                <div id="container">
-                    <div className="bgPhoto"></div>
-                </div>
-            </div>
-        )
-    }
+  componentDidMount() {
+    BgPhotoInit();
+  }
+
+  render() {
+    return (
+      <div id="bgPhoto">
+        <div id="container">
+          <div className="bgPhoto"></div>
+        </div>
+      </div>
+    );
+  }
 }
+*/
 
 export default BgPhoto;
