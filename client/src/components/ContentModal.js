f106ec810c53d9f9580850bcdbd051675d48d47a
dave <lklone2005@gmail.com>
2020-11-25 01:08:12 +0900

    add modal

--- client/src/components/ContentModal.js
+++ client/src/components/ContentModal.js
@@ -4,9 +4,11 @@ import "./ContentModal.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
+// transition효과 주기 위해 사용할 것
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-const Megabox = ({ data, exitModal }) => {
+const Modal = ({ data, exitModal }) => {
+  console.log(data);
   return (
     <div className="contentModal_megabox">
       <div className="overlay">
@@ -14,40 +16,40 @@ const Megabox = ({ data, exitModal }) => {
           <FontAwesomeIcon icon={faTimes} />
         </button>
       </div>
-      <div className="megabox">
-        <img
-          className="megaboxImg"
-          src="images/megabox_main.png"
-          alt="메가박스 이미지"
-        />
+      <div className="modal">
+        <img className="megaboxImg" src={data.img} alt={data.alt} />
       </div>
     </div>
   );
 };
 
 const ContentModal = () => {
-  const [clickedImg, setClickedImg] = useState(true);
-
-  // const [modal, setModal] = useState(false);
-  // const [modalData, setModalData] = useState("");
+  const [clickedImg, setClickedImg] = useState(false);
+  const [modalData, setModalData] = useState({});
   const [data, setData] = useState([
-    { title: "Megabox site", id: 0 },
-    { title: "Responsive site", id: 1 },
-    { title: "Something site", id: 2 },
+    {
+      id: 0,
+      title: "Megabox site",
+      img: "images/megabox_main.png",
+      alt: "메가박스 이미지",
+    },
+    {
+      id: 1,
+      title: "Responsive web",
+      img: "images/responsive-web.png",
+      alt: "반응형 웹 이미지",
+    },
+    {
+      id: 2,
+      title: "Web messenger",
+      img: "images/megabox_main.png",
+      alt: "웹 메신저 이미지",
+    },
   ]);
 
-  const project = (data) => {
-    // setModalData(data.title);
-    return (
-      <div>
-        {data.id === 0 ? (
-          <Megabox data={data} exitModal={setClickedImg} />
-        ) : null}
-
-        {/* {data.id === 1 ? <ResponsiveWeb data={data} showModal={parentShowModal}/> : null}
-      {data.id === 2 ? <Something data={data} showModal={parentShowModal}/> : null}   */}
-      </div>
-    );
+  const onClick = (data) => {
+    setClickedImg(true);
+    setModalData(data);
   };
 
   return (
@@ -60,19 +62,31 @@ const ContentModal = () => {
             <img
               src="images/express.png"
               alt="Express 이미지"
-              onClick={() => setClickedImg(true)}
+              onClick={() => onClick(data[0])}
             />
           </div>
 
+          {/* Responsive web */}
           <div className="project2">
-            <img src="images/express.png" alt="MySql 이미지" />
+            <img
+              src="images/express.png"
+              alt="MySql 이미지"
+              onClick={() => onClick(data[1])}
+            />
           </div>
 
+          {/* Web messenger */}
           <div className="project3">
-            <img src="images/express.png" alt="MySql 이미지" />
+            <img
+              src="images/express.png"
+              alt="MySql 이미지"
+              onClick={() => onClick(data[2])}
+            />
           </div>
         </div>
-        {clickedImg ? project(data[0]) : null}
+        {clickedImg ? (
+          <Modal data={modalData} exitModal={setClickedImg} />
+        ) : null}
       </div>
     </section>
   );
